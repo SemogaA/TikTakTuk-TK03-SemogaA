18fda6ce093d945e38864d8be022ebc6f5cbe956
Fauzan2125 <ahmad.fauzan45@ui.ac.id>
2026-04-28 13:27:29 +0700

    feat: implement R and CUD for artist

--- tiktaktuk/views/artist_views.py
+++ tiktaktuk/views/artist_views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from tiktaktuk.services import artist as artist_service
-
+from tiktaktuk.services.user import get_user_role_by_session
 
 def list_view(request):
+    session_id = request.COOKIES.get('session_id')
+    role = get_user_role_by_session(session_id) if session_id else None
+    
     artists = artist_service.get_all_artists()
-    return render(request, 'artist_list.html', {'artists': artists})
+    return render(request, 'artist/artist_list.html', {
+        'artists': artists, 
+        'role': role,
+        'total_artists': len(artists)
+    })
 
 
 def create_view(request):
@@ -20,9 +27,9 @@ def create_view(request):
         if artist_id:
             return redirect('artist_list')
         else:
-            return render(request, 'artist_form.html', {'error': 'gagal membuat artis.'})
+            return render(request, 'artist/artist_form.html', {'error': 'gagal membuat artis.'})
 
-    return render(request, 'artist_form.html')
+    return render(request, 'artist/artist_form.html')
 
 
 def update_view(request, artist_id):
@@ -40,10 +47,10 @@ def update_view(request, artist_id):
             return redirect('artist_list')
         else:
             artist = artist_service.get_artist_by_id(artist_id)
-            return render(request, 'artist_form.html', {'artist': artist, 'error': 'gagal update artis.'})
+            return render(request, 'artist/artist_form.html', {'artist': artist, 'error': 'gagal update artis.'})
 
     artist = artist_service.get_artist_by_id(artist_id)
-    return render(request, 'artist_form.html', {'artist': artist})
+    return render(request, 'artist/artist_form.html', {'artist': artist})
 
 
 def delete_view(request, artist_id):
