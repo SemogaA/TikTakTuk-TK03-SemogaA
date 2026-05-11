d7e3a7dab816b70e001d344b772babf31c4d3d54
mugmold <mugmold@gmail.com>
2026-05-11 22:14:11 +0700

    add csrf for railway

--- tiktaktuk/settings.py
+++ tiktaktuk/settings.py
@@ -32,6 +32,10 @@ DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.railway.app',
+]
+
 
 # Application definition
 
